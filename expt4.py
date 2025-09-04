e6418ecbee6a801fb086e0c328c1a7fed828f897
Chaitali <24csu246@ncuindia.edu>
2025-09-04 09:09:41 +0530

    Update expt4.py

--- expt4.py
+++ expt4.py
@@ -23,4 +23,30 @@ if(sum==num):
     print("Is Armstrong number")
 else:
     print("Isn't Armstrong number")
-    
+#With functions
+def prime(num):
+    # Prime number check
+    if num>1 :
+        for i in range(2, num):
+            if num % i == 0:
+                print(num, "is NOT a prime number")
+                break
+        else:
+            print(num, "is a PRIME number")#not necessary to indent ar same place as if in this case
+    else:
+        print(num, "is NOT a prime number")
+#Armstrong number
+def armstrong(num):
+    l=len(str(num))#getting length of number
+    sum=0 #initalizing sum for armstrong number
+    for i in str(num):#iterating throught string
+        i=int(i)#converting back for calculation
+        sum+=i**l #calculating armstrong number
+    #armstrong number condition and then output
+    if(sum==num):
+        print("Is Armstrong number")
+    else:
+        print("Isn't Armstrong number")
+n=int(input("Enter a number : "))
+armstrong(n)
+prime(n)
